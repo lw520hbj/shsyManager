ad59de93228794296ada499649f9be6ceb27596a
lw520hbj <2528562827@qq.com>
2022-03-06 20:53:03 +0800

    modify

--- foodbooking/models.py
+++ foodbooking/models.py
@@ -9,7 +9,7 @@ class FoodInfo(models.Model):
     food_description = models.TextField(max_length=200, null=False, verbose_name="菜品描述")
     food_location = models.CharField(max_length=30, null=False, verbose_name="食堂地址")
     food_num = models.IntegerField(null=False, verbose_name="菜品数量（份）")
-    food_price = models.IntegerField(null=False, default=10, verbose_name="菜品价格（/份）")
+    food_price = models.IntegerField(null=False, default=10, verbose_name="菜品价格（元/份）")
     food_img = models.ImageField(max_length=1000, null=False, upload_to="food_images", verbose_name="菜品图片")
     food_status = models.BooleanField(default=True, null=False, verbose_name="菜品是否上架")
     food_date = models.DateField(auto_now=False, auto_now_add=False, null=False, verbose_name="菜品上架日期")
